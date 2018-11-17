39d22c5567ba5256afa2cf893a26d922534984eb
howlXu <42797924+howlXu@users.noreply.github.com>
2018-11-18 00:26:47 +0800

    Revert "Revert "Initial commit""
    This reverts commit e79a8f8bb3096b5c70c62626fa26e3d551672818.

---
+++ src/main/java/Test.java
@@ -0,0 +1,15 @@
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+	    MyService myService = ctx.getBean(MyService.class);
+	    myService.test();
+
+	}
+
+}
